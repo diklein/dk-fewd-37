529c342c8598ab165dae5f4db8299b5e75ba20d2
David Klein <me@diklein.com>
2016-01-06 18:46:05 -0800

    Many updates
    Saved some code on homework 6.
    Using jquery on movie search now.

--- homework5/movie-search/movie-search.js
+++ homework5/movie-search/movie-search.js
@@ -1,49 +1,42 @@
 // This is what the data returning from IMDB will look like:
-var sampleResult = {
-  "Search": [
-    {
-      "Title": "Cool Runnings",
-      "Type": "movie",
-      "Year": "1993",
-      "imdbID": "tt0106611"
-    }
-  ]
-}
+// var sampleResult = {
+//   "Search": [
+//     {
+//       "Title": "Cool Runnings",
+//       "Type": "movie",
+//       "Year": "1993",
+//       "imdbID": "tt0106611"
+//     }
+//   ]
+// }
 
 // Attach an event listener to the form submit (using jQuery)
-$("#movie-search-form").submit(formSubmitted);
+//$("#movie-search-form").submit(formSubmitted);
+$("#movie-search-form").on("submit", formSubmitted);
 
 // Handle the form submission: go to OMDB and get results
 function formSubmitted(event) {
   event.preventDefault();
+  $( "#movies" ).empty();
   var url = "http://omdbapi.com/?s=" + $("#query").val();
   $.get(url, resultsReceived);
-}
+};
 
 function resultsReceived(results) {
+   console.log(results);
+   if(results.Response == "False") {
+      prompt("what the what");
+   } else {
+      for (var i = 0; i < results.Search.length; i++) {
+         var movie = results["Search"][i];
+         createMovie(movie.Title, "http://www.imdb.com/title/"+movie.imdbID, movie.Year);
+      };
+   };
+};
 
-  for (var i = 0; i < results["Search"].length; i++) {
-
-     // set up the variables
-     var ul = document.querySelector("#movies");
-     var li = document.createElement("li");
-     var divMovie = document.createElement("div");
-     var link = document.createElement("a");
-     var divYear = document.createElement("div");
-
-     // assign values
-     li.classList.add("movie");
-     divMovie.classList.add("movie-title");
-     divYear.classList.add("movie-release-date");
-     link.setAttribute("href","http://www.imdb.com/title/"+results["Search"][i]["imdbID"]);
-     link.textContent = results["Search"][i]["Title"];
-     divYear.textContent = results["Search"][i]["Year"];
-
-     // build the list
-     ul.appendChild(li);
-     li.appendChild(divMovie);
-     divMovie.appendChild(link);
-     li.appendChild(divYear);
-
-  }
-}
+function createMovie(movieTitle, url, year) {
+   var item = $("<li>").addClass("movie").appendTo("#movies");
+   var title = $("<div>").addClass("movie-title").appendTo(item);
+   $("<a>").text(movieTitle).attr("href",url).appendTo(title);
+   $("<div>").text(year).addClass("movie-release-date").appendTo(item);
+};
